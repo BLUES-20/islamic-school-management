3f957c54e932b092e3bf8d3fb1e0fe40a9d4ebc2
Ahmad Ibikunle <hammadibikunle@gmail.com>
2026-03-19 02:06:50 +0100

    Production ready: config/db.js supports DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const pool = process.env.DATABASE_URL ? new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+}) : new Pool({
     host: 'localhost',
     port: 5432,
     user: 'postgres',
