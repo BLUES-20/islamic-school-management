47df56b268f8bd2b0e4a5baa8a7e3e81400bf936
Ahmad Ibikunle <hammadibikunle@gmail.com>
2026-03-19 01:35:36 +0100

    Deploy: Switch to Supabase DB config (fixes Render PostgreSQL)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const flash = require('connect-flash');
 const path = require('path');
-const db = require('./config/db');
+const db = require('./config/supabase');
 const initDatabase = require('./init-db');
 const {
     getEmailStatus
